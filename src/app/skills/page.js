59889fa06fb1a5ac847bb5f34dc0ed826b478a78
TheStackSquad <deshcoldworld@gmail.com>
2025-10-21 12:59:43 +0100

    fixed ui

--- src/app/skills/page.js
+++ src/app/skills/page.js
@@ -7,7 +7,7 @@ import SkillUI from "@/components/skills/skillUI";
 
 const AboutPage = () => {
   return (
-    <div className="bg-[#131728] text-white font-sans min-h-screen">
+    <div className="text-black font-sans min-h-screen">
       <main className="container mx-auto px-4 md:px-8 py-16">
         <SkillUI />
       </main>
